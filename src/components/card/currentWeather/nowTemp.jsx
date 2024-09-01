cf0e11f1195b18a84068ae799ab6cb2a00cdef0f
mishab <mishab.ak@pardypanda.com>
2024-09-01 14:24:20 +0530

    if image is not there show hyphen

--- src/components/card/currentWeather/nowTemp.jsx
+++ src/components/card/currentWeather/nowTemp.jsx
@@ -7,11 +7,16 @@ function NowTemp({ selectedCountry = {}, loader = false }) {
   return (
     <div className="mt-5">
       <div className="flex flex-nowrap justify-center items-center gap-[9.5px]">
-        <img
-          className="w-14  drop-shadow-lg"
-          src={`http:${selectedCountry?.current?.icon}`}
-          alt=""
-        />
+        {selectedCountry?.current?.icon ? (
+          <img
+            className="w-14  drop-shadow-lg"
+            src={`http:${selectedCountry?.current?.icon}`}
+            alt=""
+          />
+        ) : (
+          <div className="w-14  drop-shadow-lg text-center">-</div>
+        )}
+
         <span className="text-[47.95px] leading-[58px]">
           {selectedCountry?.current?.temperature}
           <sup>°</sup>
