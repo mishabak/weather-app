a8b4d0ee5f1589e31b43d6984e91d63ef7e5a30b
mishab <mishab.ak@pardypanda.com>
2024-09-01 15:49:52 +0530

    change skelton size

--- src/components/skelton/timeIndic.jsx
+++ src/components/skelton/timeIndic.jsx
@@ -2,11 +2,11 @@ import Skeleton from "@mui/material/Skeleton";
 
 function Skelton_timeIndic() {
   return (
-    <div className="text-white p-2 flex items-center gap-[6px] flex-col justify-center text-[9.96px] ">
-      <Skeleton variant="text" width={26} height={12} className="bg-red-200" />
+    <div className="text-white flex items-center gap-[6px] flex-col justify-center text-[8px] h-[47px]">
+      <Skeleton variant="text" width={26} height={12} />
       <div className="flex flex-nowrap justify-between w-[26px]">
-        <Skeleton variant="text" width={12} className="bg-red-200" />
-        <Skeleton variant="text" width={12} className="bg-red-200" />
+        <Skeleton variant="text" width={12} />
+        <Skeleton variant="text" width={12} />
       </div>
     </div>
   );
