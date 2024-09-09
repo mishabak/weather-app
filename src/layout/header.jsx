898fb4da3a2bb467e429ebdbd44f515161e9dfc9
mishab <mishab.ak@pardypanda.com>
2024-09-09 18:39:24 +0530

    change brightness ratio for header depend on pathName

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 import { Icon_Sun } from "../components/icons";
 import { ThemeButton } from "../components";
+import { paths } from "./path";
 function Header() {
   return (
     <header className=" fixed top-0 backdrop-blur-md bg-transparent z-10 sm:h-12 md:h-16  h-12 shadow-lg w-full flex items-center justify-between px-5 md:px-7 lg:px-10">
-      <div className="flex items-center gap-9 text-currentWeather brightness-75 xs:brightness-100 text-[13px] md:text-base">
+      <div
+        className={`flex items-center gap-9 text-currentWeather ${
+          location.pathname.includes("weather")
+            ? "brightness-75"
+            : "brightness-125"
+        } xs:brightness-125 text-[13px] md:text-base`}
+      >
         <Link
           to={"/weather"}
           className="flex items-center gap-2 cursor-pointer group font-bold md:gap-3 lg:gap-4  "
@@ -13,12 +20,17 @@ function Header() {
           WEATHER APP
         </Link>
 
-        <Link
-          className="font-light hover:scale-105 hover:font-semibold duration-150"
-          to={"/list"}
-        >
-          LIST
-        </Link>
+        {paths.map(({ label, path }, idx) => (
+          <Link
+            key={idx}
+            className={`font-light hover:scale-105 hover:font-semibold duration-150 ${
+              location.pathname.includes(path) && `brightness-125`
+            }`}
+            to={path}
+          >
+            {label}
+          </Link>
+        ))}
       </div>
       <ThemeButton />
     </header>
